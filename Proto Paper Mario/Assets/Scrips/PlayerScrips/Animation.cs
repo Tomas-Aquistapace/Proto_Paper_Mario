6c71eafc80135e507e3983127d19f4aead33c4aa
Tomas <Aquista35@gmail.com>
2021-03-01 21:36:13 -0300

    Movimiento en entorno 3D

---
+++ Proto Paper Mario/Assets/Scrips/PlayerScrips/Animation.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    public Animator _animator;
+    public PlayerMovement _playerMovement;
+    public float _rotationSpeed;
+
+    bool left;
+    bool right;
+    bool lastDirection = true;
+
+    static float _ROTATION = 180;
+
+    float leftNumber = _ROTATION;
+    float rightNumber = _ROTATION;
+
+    float horizontalMove;
+    float verticalMove;
+
+    void Update()
+    {
+        RotationSprite();
+        RunAnimation();
+        JumpAnimation();
+    }
+
+    void RunAnimation()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        if (Input.GetKey("a") || Input.GetKey("d"))
+        {
+            _animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        }
+        else if (Input.GetKey("w") || Input.GetKey("s"))
+        {
+            _animator.SetFloat("Speed", Mathf.Abs(verticalMove));
+        }else
+        {
+            _animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        }
+    }
+
+    void JumpAnimation()
+    {
+        if (_playerMovement._canJump && !_playerMovement._inAir)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                _animator.SetBool("IsJumping", true);
+            }
+            _animator.SetBool("IsJumping", false);
+        }
+        else
+        {
+            _animator.SetBool("IsJumping", true);
+        }
+    }
+
+    void RotationSprite()
+    {
+        if (Input.GetKeyDown("a") || left == true)
+        {
+            if (lastDirection == true)
+            {
+                if (leftNumber > 0)
+                {
+                    transform.Rotate(new Vector3(0f, _rotationSpeed, 0f) * Time.deltaTime);
+                    left = true;
+
+                    leftNumber -= _rotationSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    left = false;
+                    leftNumber = _ROTATION;
+
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+
+                    lastDirection = false;
+                }
+            }
+        }
+        else if (Input.GetKeyDown("d") || right == true)
+        {
+            if (lastDirection == false)
+            {
+                if (rightNumber > 0)
+                {
+                    transform.Rotate(new Vector3(0f, -_rotationSpeed, 0f) * Time.deltaTime);
+                    right = true;
+
+                    rightNumber -= _rotationSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    right = false;
+                    rightNumber = _ROTATION;
+
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+
+                    lastDirection = true;
+                }
+            }
+        }
+    }
+}
---
+++ Proto Paper Mario/Assets/Scrips/PlayerScrips/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float _horizontalMove;
+    float _verticalMove;
+
+    [HideInInspector]
+    public CharacterController _charController;
+    public Ray _landingRay;
+    public RaycastHit _hit;
+
+    [SerializeField]
+    public bool _canJump;
+    public bool _inAir;
+    float _fallVelocity;
+
+    public float _playerSpeed;
+    public float _gravity;
+    public float _jumpForce;
+    public float _hightLimit;
+
+    void Start()
+    {
+        _canJump = false;
+        _inAir = false;
+        _charController = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        _horizontalMove = Input.GetAxis("Horizontal");
+        _verticalMove = Input.GetAxis("Vertical");
+
+        _landingRay = new Ray(transform.position, Vector3.down);
+        Debug.DrawRay(transform.position, Vector3.down * _hightLimit);
+
+        SetGravity();
+        PlayerJump();
+    }
+
+    private void FixedUpdate()
+    {
+        _charController.Move(new Vector3(_horizontalMove, _fallVelocity, _verticalMove) * _playerSpeed * Time.deltaTime);
+    }
+
+    private void PlayerJump()
+    {
+        if (Physics.Raycast(_landingRay, out _hit, _hightLimit) || _charController.isGrounded)
+        {
+            _canJump = true;
+            _inAir = false;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                _fallVelocity = _jumpForce;
+            }
+        }
+        else
+        {
+            _canJump = false;
+            _inAir = true;
+        }
+    }
+
+    private void SetGravity()
+    {
+        if (_charController.isGrounded)
+        {
+            _fallVelocity = -_gravity * Time.deltaTime;
+        }
+        else
+        {
+            _fallVelocity -= _gravity * Time.deltaTime;
+        }
+    }
+}
